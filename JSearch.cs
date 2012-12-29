54af3d11d065c4d0ce0d2e52684400b36a5d9156
teddy <teddy12090@gmil.com>
2012-12-29 16:44:15 +0800

    Search Model

---
+++ JSearch.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ValueThing
+{
+    class JSearch
+    {
+        public string Data = "";
+
+        [JsonProperty(PropertyName = "url")]
+        public string URL;
+
+        [JsonProperty(PropertyName = "source")]
+        public string Source;
+
+        [JsonProperty(PropertyName = "img")]
+        public string Img;
+
+        [JsonProperty(PropertyName = "title")]
+        public string Title;
+
+        [JsonProperty(PropertyName = "price")]
+        public string Price { get; set; }
+    }
+}
---
+++ Searcher.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ValueThing
+{
+    class Searcher
+    {
+        public delegate void Callback(JSearch[] jSearchs);
+
+        private string keyword;
+        private Callback CallbackMethod;
+
+        public Searcher( string keyword, Callback callback )
+        {
+            this.keyword = keyword;
+            this.CallbackMethod = callback;
+        }
+
+        public void Search()
+        {
+            string query = "http://122.117.109.65/webapi/search?keyword=" + keyword;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(query));
+            request.BeginGetResponse(WebResponseCallback, request);
+        }
+
+        private void WebResponseCallback(IAsyncResult result)
+        {
+            JSearch[] jSearchs;
+            try
+            {
+                HttpWebRequest request = ((HttpWebRequest)result.AsyncState);
+                StreamReader sr = new StreamReader(request.EndGetResponse(result).GetResponseStream());
+                string data = sr.ReadToEnd();
+                jSearchs = JsonConvert.DeserializeObject<JSearch[]>(data);               
+            }
+            catch
+            {
+                jSearchs = new JSearch[0];
+            }
+            CallbackMethod(jSearchs);
+        } 
+    }
+}
